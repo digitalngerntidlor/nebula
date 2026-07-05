687c9a1432e184cf3f43068463746663cf79403d
Pongsakorn <pongsakorn.nimphaya@gmail.com>
2026-07-05 14:05:43 +0700

    Stop tracking nebula/tests folder

--- nebula/tests/defs/test_resources.py
+++
@@ -1,6 +0,0 @@
-from nebula.defs.resources import resources
-
-
-def test_resources_definitions_expose_duckdb_and_bigquery():
-    defs = resources()
-    assert set(defs.resources.keys()) == {"duckdb", "bigquery"}
